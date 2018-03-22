3993e77ee88318dc4c457f81ea62dd5ed00af061
minto <minto@hotmail.fr>
2018-03-22 08:51:33 +0100

    graph coloring largest degree first part 1

--- solver.py
+++ solver.py
@@ -77,3 +77,31 @@ class Solver:
                         links.append({"from": traffic1.name, "to": traffic2.name})
             visited.add(traffic1)
         return {'nodes': nodes, 'links': links}
+
+    def largest_degree_first(self):
+        # we color the transformed graph by allocating colors to largest
+        # degree nodes:
+        # 1) we select the largest degree uncolored optical switch
+        # 2) we look at the adjacent vertices and select the minimum indexed
+        # color not yet used by adjacent vertices
+        # 3) when everything is colored, we stop
+        
+        # we will use a dictionary that binds traffic to the color it uses.
+        traffic_color = dict.fromkeys(Node.query.all(), None)
+        # and a list that contains all vertices that we have yet to color
+        uncolored_nodes = list(optical_switch_color)
+        # we will use a function that returns the degree of a node to sort
+        # the list in ascending order
+        uncolored_nodes.sort(key=lambda node: len(node.adjacencies())
+        # and pop nodes one by one
+        while uncolored_nodes:
+            largest_degree = uncolored_nodes.pop()
+            # we compute the set of colors used by neighbors
+            colors = set(traffic_color[n] for n, _ in largest_degree.adjacencies('fiber'))
+            # we find the minimum indexed color which is available
+            min_index = [i in colors for i in range(len(colors) + 1)].index(0)
+            # and assign it to the current optical switch
+            optical_switch_color[largest_degree] = min_index
+            
+        number_lambda = max(optical_switch_color.values()) + 1
+        return number_lambda
